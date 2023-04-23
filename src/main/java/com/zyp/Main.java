2c1e0b46ef7f8dfa7853532db62cad62726d6f1e
yp.zhang <1185164950@qq.com>
2023-04-23 18:30:52 +0800

    初始化一个maven项目

---
+++ src/main/java/com/zyp/Main.java
@@ -0,0 +1,11 @@
+package com.zyp;
+
+/**
+ * @author yp.zhang
+ * @date 2023/4/23
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
